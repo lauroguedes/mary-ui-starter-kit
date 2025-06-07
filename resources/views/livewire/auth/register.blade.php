9c275f62c57e65048b446d43efdfd045f2ad061b
Lauro Guedes <leowgweb@gmail.com>
2025-06-07 17:45:25 +0100

    refactor: update authentication layout component from split to card and enhance password validation rules

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -19,10 +19,12 @@ new #[Layout('components.layouts.auth')] class extends Component {
      */
     public function register(): void
     {
+        $passwordRules = app()->isProduction() ? ['required', 'string', 'confirmed', Rules\Password::defaults()] : ['required', 'string', 'confirmed'];
+
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+            'password' => $passwordRules,
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
