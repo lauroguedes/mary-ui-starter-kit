a8c82a3af4358232c3a01e8ecf558b28d539ad3a
Lauro Guedes <leowgweb@gmail.com>
2025-06-07 23:21:45 +0100

    feat: add blade-fontawesome dependency and refactor layout components for improved structure and styling

---
+++ resources/views/livewire/settings/user-menu.blade.php
@@ -0,0 +1,47 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Livewire\Actions\Logout;
+use App\Models\User;
+
+new class extends Component {
+    public User $user;
+
+    public function mount(): void
+    {
+        $this->user = auth()->user();
+    }
+
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
+<div>
+    <x-mary-menu-separator />
+
+    <x-mary-list-item :item="$user" value="name" sub-value="email" no-separator no-hover
+        class="-mx-2 !-my-2 rounded">
+        <x-slot:avatar>
+            <x-mary-avatar :placeholder="$user->initials()" class="!w-10" />
+        </x-slot:avatar>
+        <x-slot:actions>
+            <x-mary-dropdown>
+                <x-slot:trigger>
+                    <x-mary-button icon="m-cog-6-tooth" class="btn-circle" />
+                </x-slot:trigger>
+                <x-mary-menu-item :title="__('Profile')" icon="c-user" :link="route('settings.profile')" />
+                <x-mary-menu-item :title="__('Repository')" icon="fab.github" link="https://laravel.com/docs/starter-kits"
+                    external />
+                <x-mary-menu-item :title="__('Documentation')" icon="s-book-open" link="https://laravel.com/docs/starter-kits"
+                    external />
+                <x-mary-menu-item :title="__('Log out')" wire:click.stop="logout" spinner="logout" class="text-error"
+                    icon="o-power" />
+            </x-mary-dropdown>
+        </x-slot:actions>
+    </x-mary-list-item>
+
+    <x-mary-menu-separator />
+</div>
