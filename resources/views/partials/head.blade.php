1f23cbf08ccad913b0b888dcb46db55d42762066
Lauro Guedes <leowgweb@gmail.com>
2025-06-11 17:20:14 +0100

    refactor: enhance dashboard layout and improve head metadata handling

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,12 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'Laravel' }}</title>
+<title>
+    {{ $title ?? config('app.name', 'Laravel') }}
+    @if (isset($title))
+        - {{ config('app.name', 'Laravel') }}
+    @endif
+</title>
+<meta name="description" content="{{ $metaDescription ?? config('app.description', 'Default description') }}">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
