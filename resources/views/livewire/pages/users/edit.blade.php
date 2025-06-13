231bfe97d3071e6eb1dbda3fc97f8c2cff1ef395
Lauro Guedes <leowgweb@gmail.com>
2025-06-13 11:45:48 +0100

    feat: implement user edit functionality with validation and form handling

--- resources/views/livewire/pages/users/edit.blade.php
+++ resources/views/livewire/pages/users/edit.blade.php
@@ -3,13 +3,46 @@
 use Livewire\Volt\Component;
 use App\Models\User;
 use Mary\Traits\Toast;
+use Livewire\Attributes\Rule;
 
 new class extends Component {
     use Toast;
 
     public User $user;
+
+    #[Rule('required|max:100')]
+    public string $name = '';
+
+    #[Rule('required|email|max:50')]
+    public string $email = '';
+
+    public function mount(): void
+    {
+        $this->fill($this->user);
+    }
+
+    public function save(): void
+    {
+        $data = $this->validate();
+
+        $this->user->update($data);
+
+        $this->success('User updated with success.', redirectTo: route('users.index'));
+    }
+
 }; ?>
 
-<x-pages.layout :title="__('Update') . ' - ' . $user->name">
-    update
+<x-pages.layout :page-title="__('Update') . ' - ' . $user->name">
+    <x-slot:content>
+        <x-mary-form wire:submit="save">
+            <x-mary-input :label="__('Name')" wire:model="name" />
+            <x-mary-input :label="__('Email')" wire:model="email" />
+
+            <x-slot:actions>
+                <x-mary-button :label="__('Cancel')" :link="route('users.index')" class="btn-soft" />
+                <x-mary-button :label="__('Save')" icon="o-paper-airplane" spinner="save" type="submit"
+                    class="btn-primary" />
+            </x-slot:actions>
+        </x-mary-form>
+    </x-slot:content>
 </x-pages.layout>
